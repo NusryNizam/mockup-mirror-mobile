bbac2c95b7f93b8d7d4c152d16ca08e78fcf9fb7
Nusry <nusry.n@eatmeglobal.com>
2024-11-25 06:32:48 +0530

    layout changes

--- App.tsx
+++ App.tsx
@@ -1,59 +1,29 @@
-
- 
-import { ThemedButton } from './src/components/ThemedButton';
-import { ThemedText } from './src/components/ThemedText';
-import { ThemedView } from './src/components/ThemedView';
-import { CameraView, ScanningResult, useCameraPermissions } from 'expo-camera';
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {ThemedButton} from "./src/components/ThemedButton";
+import {ThemedText} from "./src/components/ThemedText";
+import {ThemedView} from "./src/components/ThemedView";
+import {CameraView, ScanningResult, useCameraPermissions} from "expo-camera";
+import React from "react";
 import {
+  ActivityIndicator,
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
-} from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.dark.text : Colors.light.text,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light.text : Colors.dark.text,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+} from "react-native";
+import {Colors} from "./src/constants/Colors";
 
 function App(): React.JSX.Element {
   const [permission, requestPermission] = useCameraPermissions();
 
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.dark.background : Colors.light.background,
+    backgroundColor: isDarkMode
+      ? Colors.dark.background
+      : Colors.light.background,
   };
+
+  const loaderColor = isDarkMode ? Colors.dark.tint : Colors.light.tint;
 
   const handleScan = (result: ScanningResult) => {
     console.log("Scanned: ", result);
@@ -61,17 +31,9 @@ function App(): React.JSX.Element {
 
   if (!permission) {
     // Camera permissions are still loading.
-    return <ThemedView />;
-  }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
-      <ThemedView style={styles.container}>
-        <ThemedText style={styles.message}>
-          We need your permission to show the camera
-        </ThemedText>
-        <ThemedButton text="Grant permission" onPress={requestPermission} />
+      <ThemedView>
+        <ActivityIndicator color={loaderColor} />
       </ThemedView>
     );
   }
@@ -79,13 +41,14 @@ function App(): React.JSX.Element {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Setup Your Connection</ThemedText>
         <ThemedText>Scan the QR Code to get started</ThemedText>
         <ThemedText></ThemedText>
+
         <CameraView
           facing="back"
           barcodeScannerSettings={{
@@ -94,6 +57,16 @@ function App(): React.JSX.Element {
           onBarcodeScanned={handleScan}
           style={styles.camera}
         />
+
+        {!permission.granted ? (
+          <ThemedView>
+            <ThemedText></ThemedText>
+            <ThemedText style={styles.message}>
+              We need your permission to show the camera
+            </ThemedText>
+            <ThemedButton text="Grant permission" onPress={requestPermission} />
+          </ThemedView>
+        ) : null}
       </ThemedView>
     </SafeAreaView>
   );
@@ -106,15 +79,15 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
   stepContainer: {
     padding: 24,
@@ -128,14 +101,12 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
 
-
   container: {
     flex: 1,
     justifyContent: "center",
     padding: 24,
   },
   message: {
-    textAlign: "center",
     paddingBottom: 10,
   },
 });
