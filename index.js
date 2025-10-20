3fa946ec63ad0eea1771ed213345b0268a2a0a3c
Nusry Nizam <nusrynizam@gmail.com>
2025-10-20 19:46:56 +0530

    refactor: upgrade third party packages

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import SystemNavigationBar from 'react-native-system-navigation-bar';
+import { name as appName } from './app.json';
 
-SystemNavigationBar.navigationHide();
 AppRegistry.registerComponent(appName, () => App);
